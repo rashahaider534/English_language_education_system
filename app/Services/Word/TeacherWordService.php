0e4bcbbe54e348cce1d73e778458f8cb0ef2c215
rashahaider534 <rsha74877@gmail.com>
2026-08-16 17:11:03 +0300

    teacher word

--- app/Services/Word/TeacherWordService.php
+++ app/Services/Word/TeacherWordService.php
@@ -9,62 +9,68 @@ use App\Enums\ContentStatus;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+
 class TeacherWordService
 {
     public function create(Lesson $lesson, array $data)
     {
-         return DB::transaction(function () use ($lesson, $data) {
-        if (!in_array($lesson->status, [
-            ContentStatus::DRAFT,
-            ContentStatus::PENDING,
-            ContentStatus::CHANGES_REQUESTED
-        ])) {
-            throw ValidationException::withMessages([
-                'word' => 'You cannot add word to this lesson.',
-            ]);
-        }
-        $word = Word::create(
-            [
-                'lesson_id' => $lesson->id,
-                'word_en' => $data['word_en'],
-                'word_ar' => $data['word_ar']
-            ]
-        );
-        if (isset($data['audio'])) {
+        return DB::transaction(function () use ($lesson, $data) {
+            if (!in_array($lesson->status, [
+                ContentStatus::DRAFT,
+                ContentStatus::PENDING,
+                ContentStatus::CHANGES_REQUESTED
+            ])) {
+                throw ValidationException::withMessages([
+                    'word' => 'You cannot add word to this lesson.',
+                ]);
+            }
+            $word = Word::create(
+                [
+                    'lesson_id' => $lesson->id,
+                    'word_en' => $data['word_en'],
+                    'word_ar' => $data['word_ar']
+                ]
+            );
+            if (isset($data['audio'])) {
                 $word
                     ->addMedia($data['audio'])
                     ->toMediaCollection('audios');
             }
-        return $word->load('media');
-         });
+            return $word->load('media');
+        });
     }
 
     public function update(Word $word, array $data)
     {
         return DB::transaction(function () use ($word, $data) {
-        if (!in_array($word->lesson->status, [
-            ContentStatus::DRAFT,
-            ContentStatus::PENDING,
-            ContentStatus::CHANGES_REQUESTED
-        ])) {
-            throw ValidationException::withMessages([
-                'word' => 'You cannot update word .',
-            ]);
-        }
+            if (!in_array($word->lesson->status, [
+                ContentStatus::DRAFT,
+                ContentStatus::PENDING,
+                ContentStatus::CHANGES_REQUESTED
+            ])) {
+                throw ValidationException::withMessages([
+                    'word' => 'You cannot update word .',
+                ]);
+            }
 
-        if (isset($data['audio'])) {
-             $word->clearMediaCollection('audios');
+            if (isset($data['audio'])) {
+                $word->clearMediaCollection('audios');
                 $word
                     ->addMedia($data['audio'])
                     ->toMediaCollection('audios');
             }
-        $word->update(
-            [
-                'word_en' => $data['word_en'],
-                'word_ar' => $data['word_ar']
-            ]
-        );
-        return $word->fresh();
+            $updateData = [];
+
+            if (isset($data['word_en'])) {
+                $updateData['word_en'] = $data['word_en'];
+            }
+
+            if (isset($data['word_ar'])) {
+                $updateData['word_ar'] = $data['word_ar'];
+            }
+
+            $word->update($updateData);
+            return $word->fresh();
         });
     }
 
